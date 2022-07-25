7b7e451d691711af872d9ea6fd3297bb3964385e
_arbiz1k Tim <tim.obozny@gmail.com>
2022-07-25 11:25:56 +0300

    Upload main files

---
+++ Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿
+namespace crossesAndZeros
+{
+    partial class trainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.b9 = new System.Windows.Forms.Button();
+            this.b8 = new System.Windows.Forms.Button();
+            this.b7 = new System.Windows.Forms.Button();
+            this.b6 = new System.Windows.Forms.Button();
+            this.b5 = new System.Windows.Forms.Button();
+            this.b4 = new System.Windows.Forms.Button();
+            this.b3 = new System.Windows.Forms.Button();
+            this.b2 = new System.Windows.Forms.Button();
+            this.b1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.label1.Font = new System.Drawing.Font("Roboto Bk", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.Cyan;
+            this.label1.Location = new System.Drawing.Point(61, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(329, 44);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Crosses And Zeros";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Roboto Bk", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.ForeColor = System.Drawing.Color.Cyan;
+            this.button1.Location = new System.Drawing.Point(420, -1);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(32, 29);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "X";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.b9);
+            this.panel1.Controls.Add(this.b8);
+            this.panel1.Controls.Add(this.b7);
+            this.panel1.Controls.Add(this.b6);
+            this.panel1.Controls.Add(this.b5);
+            this.panel1.Controls.Add(this.b4);
+            this.panel1.Controls.Add(this.b3);
+            this.panel1.Controls.Add(this.b2);
+            this.panel1.Controls.Add(this.b1);
+            this.panel1.Location = new System.Drawing.Point(12, 98);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(426, 340);
+            this.panel1.TabIndex = 2;
+            // 
+            // b9
+            // 
+            this.b9.Font = new System.Drawing.Font("Roboto Bk", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.b9.Location = new System.Drawing.Point(321, 241);
+            this.b9.Name = "b9";
+            this.b9.Size = new System.Drawing.Size(84, 81);
+            this.b9.TabIndex = 8;
+            this.b9.Text = "*";
+            this.b9.UseVisualStyleBackColor = true;
+            this.b9.Click += new System.EventHandler(this.b9_Click);
+            // 
+            // b8
+            // 
+            this.b8.Font = new System.Drawing.Font("Roboto Bk", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.b8.Location = new System.Drawing.Point(171, 241);
+            this.b8.Name = "b8";
+            this.b8.Size = new System.Drawing.Size(84, 81);
+            this.b8.TabIndex = 7;
+            this.b8.Text = "*";
+            this.b8.UseVisualStyleBackColor = true;
+            this.b8.Click += new System.EventHandler(this.b8_Click);
+            // 
+            // b7
+            // 
+            this.b7.Font = new System.Drawing.Font("Roboto Bk", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.b7.Location = new System.Drawing.Point(21, 241);
+            this.b7.Name = "b7";
+            this.b7.Size = new System.Drawing.Size(84, 81);
+            this.b7.TabIndex = 6;
+            this.b7.Text = "*";
+            this.b7.UseVisualStyleBackColor = true;
+            this.b7.Click += new System.EventHandler(this.b7_Click);
+            // 
+            // b6
+            // 
+            this.b6.Font = new System.Drawing.Font("Roboto Bk", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.b6.Location = new System.Drawing.Point(321, 130);
+            this.b6.Name = "b6";
+            this.b6.Size = new System.Drawing.Size(84, 81);
+            this.b6.TabIndex = 5;
+            this.b6.Text = "*";
+            this.b6.UseVisualStyleBackColor = true;
+            this.b6.Click += new System.EventHandler(this.b6_Click);
+            // 
+            // b5
+            // 
+            this.b5.Font = new System.Drawing.Font("Roboto Bk", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.b5.Location = new System.Drawing.Point(171, 130);
+            this.b5.Name = "b5";
+            this.b5.Size = new System.Drawing.Size(84, 81);
+            this.b5.TabIndex = 4;
+            this.b5.Text = "*";
+            this.b5.UseVisualStyleBackColor = true;
+            this.b5.Click += new System.EventHandler(this.b5_Click);
+            // 
+            // b4
+            // 
+            this.b4.Font = new System.Drawing.Font("Roboto Bk", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.b4.Location = new System.Drawing.Point(21, 130);
+            this.b4.Name = "b4";
+            this.b4.Size = new System.Drawing.Size(84, 81);
+            this.b4.TabIndex = 3;
+            this.b4.Text = "*";
+            this.b4.UseVisualStyleBackColor = true;
+            this.b4.Click += new System.EventHandler(this.b4_Click);
+            // 
+            // b3
+            // 
+            this.b3.Font = new System.Drawing.Font("Roboto Bk", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.b3.Location = new System.Drawing.Point(321, 24);
+            this.b3.Name = "b3";
+            this.b3.Size = new System.Drawing.Size(84, 81);
+            this.b3.TabIndex = 2;
+            this.b3.Text = "*";
+            this.b3.UseVisualStyleBackColor = true;
+            this.b3.Click += new System.EventHandler(this.b3_Click);
+            // 
+            // b2
+            // 
+            this.b2.Font = new System.Drawing.Font("Roboto Bk", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.b2.Location = new System.Drawing.Point(171, 24);
+            this.b2.Name = "b2";
+            this.b2.Size = new System.Drawing.Size(84, 81);
+            this.b2.TabIndex = 1;
+            this.b2.Text = "*";
+            this.b2.UseVisualStyleBackColor = true;
+            this.b2.Click += new System.EventHandler(this.b2_Click);
+            // 
+            // b1
+            // 
+            this.b1.Font = new System.Drawing.Font("Roboto Bk", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.b1.Location = new System.Drawing.Point(21, 24);
+            this.b1.Name = "b1";
+            this.b1.Size = new System.Drawing.Size(84, 81);
+            this.b1.TabIndex = 0;
+            this.b1.Text = "*";
+            this.b1.UseVisualStyleBackColor = true;
+            this.b1.Click += new System.EventHandler(this.b1_Click);
+            // 
+            // trainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.CadetBlue;
+            this.ClientSize = new System.Drawing.Size(452, 450);
+            this.ControlBox = false;
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.Name = "trainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.trainForm_Load);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button b9;
+        private System.Windows.Forms.Button b8;
+        private System.Windows.Forms.Button b7;
+        private System.Windows.Forms.Button b6;
+        private System.Windows.Forms.Button b5;
+        private System.Windows.Forms.Button b4;
+        private System.Windows.Forms.Button b3;
+        private System.Windows.Forms.Button b2;
+        private System.Windows.Forms.Button b1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace crossesAndZeros
+{
+    public partial class trainForm : Form
+    {
+        public trainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        private bool prevSymb = false;
+        private int clickCounter = 0;
+
+        private void ChangeChoise(string whatButton)
+        {
+            clickCounter++;
+            if (prevSymb == false)
+            {
+                ChangeButtonCaption(whatButton, "X");
+                prevSymb = true;
+            }
+            else
+            {
+                ChangeButtonCaption(whatButton, "O");
+                prevSymb = false;
+            }
+        }
+        private void ChangeButtonCaption(string whatButton, string symb)
+        {
+            if (whatButton == "b1")
+            {
+                b1.Text = symb;
+                FindWinner(symb);
+            }
+            else if (whatButton == "b2")
+            {
+                b2.Text = symb;
+                FindWinner(symb);
+            }
+            else if (whatButton == "b3")
+            {
+                b3.Text = symb;
+                FindWinner(symb);
+            }
+            else if (whatButton == "b4")
+            {
+                b4.Text = symb;
+                FindWinner(symb);
+            }
+            else if (whatButton == "b5")
+            {
+                b5.Text = symb;
+                FindWinner(symb);
+            }
+            else if (whatButton == "b6")
+            {
+                b6.Text = symb;
+                FindWinner(symb);
+            }
+            else if (whatButton == "b7")
+            {
+                b7.Text = symb;
+                FindWinner(symb);
+            }
+            else if (whatButton == "b8")
+            {
+                b8.Text = symb;
+                FindWinner(symb);
+            }
+            else if (whatButton == "b9")
+            {
+                b9.Text = symb;
+                FindWinner(symb);
+            }
+        }
+
+        private void FindWinner(string symb)
+        {
+            //horizontal
+            if (b1.Text == symb && b2.Text == symb && b3.Text == symb)
+            {
+                MessageBox.Show($"Winner is {symb} !!!");
+                Application.Restart();
+            }
+            if (b4.Text == symb && b5.Text == symb && b6.Text == symb)
+            {
+                MessageBox.Show($"Winner is {symb} !!!");
+                Application.Restart();
+            }
+            if (b7.Text == symb && b8.Text == symb && b9.Text == symb)
+            {
+                MessageBox.Show($"Winner is {symb} !!!");
+                Application.Restart();
+            }
+            //vertical
+            if (b1.Text == symb && b4.Text == symb && b7.Text == symb)
+            {
+                MessageBox.Show($"Winner is {symb} !!!");
+                Application.Restart();
+            }
+            if (b2.Text == symb && b5.Text == symb && b8.Text == symb)
+            {
+                MessageBox.Show($"Winner is {symb} !!!");
+                Application.Restart();
+            }
+            if (b3.Text == symb && b6.Text == symb && b9.Text == symb)
+            {
+                MessageBox.Show($"Winner is {symb} !!!");
+                Application.Restart();
+            }
+            //leftToRight
+            if (b1.Text == symb && b5.Text == symb && b9.Text == symb)
+            {
+                MessageBox.Show($"Winner is {symb} !!!");
+                Application.Restart();
+            }
+            //rightToLeft
+            if (b3.Text == symb && b5.Text == symb && b7.Text == symb)
+            {
+                MessageBox.Show($"Winner is {symb} !!!");
+                Application.Restart();
+            }
+            //nobodyWon
+            if (clickCounter == 9)
+            {
+                MessageBox.Show($"Nobody won *_* ");
+                Application.Restart();
+            }
+        }
+
+        private void b1_Click(object sender, EventArgs e)
+        {
+            b1.BackColor = Color.Aquamarine;
+            ChangeChoise("b1");
+        }
+
+        private void b2_Click(object sender, EventArgs e)
+        {
+            b2.BackColor = Color.Aquamarine;
+            ChangeChoise("b2");
+        }
+
+        private void b3_Click(object sender, EventArgs e)
+        {
+            b3.BackColor = Color.Aquamarine;
+            ChangeChoise("b3");
+        }
+
+        private void b4_Click(object sender, EventArgs e)
+        {
+            b4.BackColor = Color.Aquamarine;
+            ChangeChoise("b4");
+        }
+
+        private void b5_Click(object sender, EventArgs e)
+        {
+            b5.BackColor = Color.Aquamarine;
+            ChangeChoise("b5");
+        }
+
+        private void b6_Click(object sender, EventArgs e)
+        {
+            b6.BackColor = Color.Aquamarine;
+            ChangeChoise("b6");
+        }
+
+        private void b7_Click(object sender, EventArgs e)
+        {
+            b7.BackColor = Color.Aquamarine;
+            ChangeChoise("b7");
+        }
+
+        private void b8_Click(object sender, EventArgs e)
+        {
+            b8.BackColor = Color.Aquamarine;
+            ChangeChoise("b8");
+        }
+
+        private void b9_Click(object sender, EventArgs e)
+        {
+            b9.BackColor = Color.Aquamarine;
+            ChangeChoise("b9");
+        }
+
+        private void trainForm_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Let's play :/\nYou start from X");
+        }
+    }
+}
